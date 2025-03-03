df7342ede5b5c3cd2a98a8a4878c66e2d6e42ff5
aprilossai <135135277+aprilossai@users.noreply.github.com>
2025-03-03 12:37:30 -0500

    Add files via upload

--- Anova-test.R
+++ Anova-test.R
@@ -8,3 +8,16 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 
 #read dataset in a variable 
 df <- read.csv(PATH)
+
+#Understand levels/classes in datasets
+factor(df$treat))
+
+#Applying ANOVA function
+> anova_one_way <- aov(time~poison, data = df)
+> summary(anova_one_way)
+
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
